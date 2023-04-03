b07c4925a0fce1e5f1152e9925392a3d095d0be6
SaveliiYam <98161580+SaveliiYam@users.noreply.github.com>
2023-04-03 11:14:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "Client.h"
+
+int main()
+{
+    Client client;
+    std::string res = client.getResponse("yandex.ru");
+    std::string field;
+    std::cin >> field;
+    std::cout << field << ": '" << client.getFieldFromJsonn(res, field) << "'";
+    return 0;
+}
